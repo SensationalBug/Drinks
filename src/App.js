efbb6bbb03598b900f4db4320aba31799c532e21
Pedro Luis De Leon <breidydl@gmail.com>
2022-02-09 16:24:26 -0400

    Agregar busqueda

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Form } from "./components/Form";
-import CategoriasProvider from "./components/CategoriasContext";
 import "./App.css";
+
+import CategoriasProvider from "./context/CategoriasContext";
+import RecetasProvider from "./context/RecetasContext";
 
 function App() {
   return (
     <CategoriasProvider>
-      <div className="justify-content-center container">
-        <header className="bg-info text-center py-2 text-white">
-          Buscador de Bebidas
-        </header>
-        <Form />
-      </div>
+      <RecetasProvider>
+        <div className="justify-content-center container">
+          <header className="bg-info text-center py-2 text-white">
+            Buscador de Bebidas
+          </header>
+          <Form />
+        </div>
+      </RecetasProvider>
     </CategoriasProvider>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,43 @@
-import React, { useContext } from "react";
-import { CategoriasContext } from "./CategoriasContext";
+import React, { useContext, useState } from "react";
+import { CategoriasContext } from "../context/CategoriasContext";
+import { RecetasContext } from "../context/RecetasContext";
 
 export const Form = () => {
+  const [buscar, setBuscar] = useState({
+    nombre: "",
+    categoria: "",
+  });
   const { categorias } = useContext(CategoriasContext);
+  const { setBusqueda } = useContext(RecetasContext);
+
+  const obtenerData = (e) => {
+    setBuscar({
+      ...buscar,
+      [e.target.name]: [e.target.value],
+    });
+  };
   return (
-    <form className="form row mt-5">
+    <form
+      className="form row mt-5"
+      onSubmit={(e) => {
+        e.preventDefault();
+        setBusqueda(buscar);
+      }}
+    >
       <div className="col-md-4">
-        <input type="text" name="nombre" className="form-control" />
+        <input
+          type="text"
+          name="nombre"
+          className="form-control"
+          onChange={obtenerData}
+        />
       </div>
       <div className="col-md-4">
-        <select className="form-control">
+        <select
+          className="form-control"
+          name="categoria"
+          onChange={obtenerData}
+        >
           <option value="">-- Seleccionar --</option>
           {categorias.map((element) => (
             <option key={element.strCategory} value={element.strCategory}>
@@ -20,7 +48,7 @@ export const Form = () => {
       </div>
       <div className="col-md-4">
         <input
-          type="button"
+          type="submit"
           className="form-control bg-info text-white"
           value="Buscar"
         />
